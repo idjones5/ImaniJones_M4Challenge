d145375cff547f53c8076f0cbe64055de21f8dfa
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-02-06 01:22:45 -0800

    Updated files

--- src/main/java/com/company/validNumber/SomeMath.java
+++ src/main/java/com/company/validNumber/SomeMath.java
@@ -1,2 +1,27 @@
-package com.company.validNumber;public class SomeMath {
+package com.company.validNumber;
+
+public class SomeMath {
+
+    public static int total5(int x, int y, int z, int a, int b){
+        return x + y + z + a + b;
+    }
+
+    public static int average5(int x, int y, int z, int a, int b){
+        double avg = ((x + y + z + a + b) / 5);
+        return (int) avg;
+    }
+
+    public static double largest5(double x, double y, double z, double a, double b){
+        return Math.max(Math.max(Math.max(Math.max(x,y),z),a),b);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(total5(1,2,3,4,5));
+        System.out.println(average5(1,3,5,7,9));
+        System.out.println(largest5(42.0,35.1,2.3,40.2,15.6));
+
+    }
 }
--- src/main/java/com/company/validNumber/ValidNumber.java
+++ src/main/java/com/company/validNumber/ValidNumber.java
@@ -1,2 +1,24 @@
-package com.company.validNumber;public class ValidNumber {
+package com.company.validNumber;
+
+import java.util.Scanner;
+
+public class ValidNumber {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 10: ");
+        int userInput = scan.nextInt();
+
+        while (userInput < 1 || userInput > 10) {
+
+            System.out.println("You must enter a number between 1 and 10. Please try again");
+
+            System.out.println("Enter a number between 1 and 10: ");
+            userInput = scan.nextInt();
+        }
+
+        System.out.print("\nThe number you entered is: " + userInput);
+
+    }
 }
